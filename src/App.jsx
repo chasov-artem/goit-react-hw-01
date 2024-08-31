5de7f5c47688250ffdf1c3cf8e7020ee75969d5d
chasov-artem <chasov90@gmail.com>
2024-08-31 16:02:22 +0300

    second task save

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Profile from "./assets/components/Profile/Profile.jsx";
 import userData from "./userData.json";
+import friends from "../friends.json";
+import FriendList from "./assets/components/FriendList/FriendList.jsx";
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </div>
   );
 };
